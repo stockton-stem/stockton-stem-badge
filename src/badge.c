954facd37b4e1f8d4ad17165cf345c5d8138a780
Chris Luke <chrisy@flirble.org>
2017-05-26 18:33:02 -0400

    Experiment with PWM values

--- src/badge.c
+++ src/badge.c
@@ -5,24 +5,40 @@
  * https://github.com/stockton-stem/stockton-stem-badge
  */
 
+#include "config.h"
 #include <xc.h>
 #include "badge.h"
 
-#define LED1 PWM1DCH
-#define LED2 PWM2DCH
-#define LED3 PWM3DCH
-#define LED4 PWM4DCH
+
+// Programs all 10 bits of the PWM duty cycle
+// Not convinced this is quite right, yet
+#define set_pwm10(pwm, val) do { \
+    pwm##DCH = (unsigned char)(val >> 2); \
+    pwm##DCL = (unsigned char)(val << 6); \
+} while(0)
+
+// Programs only the 8MSB of the PWM duty cycle
+#define set_pwm8(pwm, val) do { \
+    pwm##DCH = (unsigned char)val; \
+} while(0)
 
 void badge_iterate(void) {
     // Animate the badge here!
-    static int i = 0;
-    
-    LED1 = i & 0xff;;
-    LED2 = (i + 32) & 0xff;
-    LED3 = (i + 64) & 0xff;
-    LED4 = (i + 96) & 0xff;
+    static int i = 0, j = 16, k = 32, l = 64;
 
-    ++i;
+    set_pwm8(PWM1, i>>2);
+    set_pwm8(PWM2, j);
+    set_pwm8(PWM3, k);
+    set_pwm8(PWM4, l);
+
+    ++i; ++j; ++k; ++l;
+    
+    if (i > 127) i = 0;
+    if (j > 127) j = 0;
+    if (k > 127) k = 0;
+    if (l > 127) l = 0;
+
+    __delay_ms(10);
 
     return;
 }
